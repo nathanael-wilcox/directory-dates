503842f9bd85a7a536b10fe7d12c75e2c9a7e256
nathanael-wilcox <nathanaelwilcox@outlook.com>
2025-11-17 21:49:51 -0600

    init

---
+++ index.js
@@ -0,0 +1,62 @@
+const { MongoClient } = require("mongodb");
+const mongoose = require("mongoose");
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const personSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ["Single", "Dating", "Married"],
+  },
+  partner: {
+    type: String,
+  },
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+mongoose.connect(process.env.DB_URL);
+
+async function getPerson(email) {
+  const result = await Person.find();
+  return result;
+}
+
+app.post("/user", async (req, res, next) => {
+  console.log(req.body);
+  const r = await getPerson("nthnlwlcx23@dordt.edu");
+  res.send(r);
+});
+
+app.post("/get", async (req, res, next) => {
+  let { email, id } = req.body;
+});
+
+app.post("/create", async (req, res, next) => {
+  let { name, email, status, id } = req.body;
+  const person = new Person({
+    name,
+    email,
+    status,
+  });
+  await person.save();
+  res.redirect("/");
+});
+
+app.listen(3000, () => {
+  console.log("Running");
+});
